eefe2f00b52009f2c6e1034653c998208c0fc15e
Girma Nigusse <xgirma@gmail.com>
2020-04-12 20:42:12 -0400

    update

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
-{
-  "setupFilesAfterEnv": [
-    "./jest.setup.js"
-  ],
-  "testEnvironment": "node",
-  "testMatch": [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
-  ],
-  "testPathIgnorePatterns": [
-    "/node_modules/"
-  ]
-}
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  transform: {
+    "^.+\.tsx?$": "ts-jest"
+  }
+};
